ce9928765cfce71ef729e8291931411c6b77fa8a
Monny Chou <firemonny@hotmail.com>
2018-09-17 22:04:45 -0600

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //configure parameter
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 14f;
+    bool hasStarted = false;
+    //state
+    Vector2 paddleToBallVector;
+    // Use this for initialization
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LauchOnMouseClick();
+        }
+    }
+
+    private void LauchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //configuration parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 0f;
+    [SerializeField] float maxX = 16f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnit = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(mousePosInUnit, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
